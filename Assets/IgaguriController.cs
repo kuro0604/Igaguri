95ad57e1e10df53e56e345947f80fafc7a5412d3
kuro0604 <daiya21yu@gmail.com>
2020-09-02 11:48:21 +0900

    初回コミット

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    public int point;
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+        GetComponent<SphereCollider>().enabled = false;
+        GameObject.FindGameObjectWithTag("Canvas").GetComponent<ScoreText>().AddPoint(point);
+        Debug.Log("Igaguri");
+
+    }
+    void Start()
+    {
+        //shoot(new Vector3(0, 200, 2000)); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ScoreText.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highScoreText;
+
+    private int score;
+    private int highScore;
+
+    private string highScoreKey = "highScore";
+
+    void Start()
+    {
+        Initialize();
+        //scoreText.color = new Color(0.5f, 0.5f, 0.5f);
+    }
+
+    /// <summary>
+    /// 数値の初期化
+    /// </summary>
+    private void Initialize()
+    {
+        score = 0;
+
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+  
+    /// <summary>
+    /// 加点
+    /// </summary>
+    /// <param name="point">加算するポイント</param>
+    public void AddPoint (int point)
+    {
+        score += point;
+
+        Debug.Log(score);
+
+        if(highScore < score)
+        {
+            highScore = score;
+
+            Debug.Log(highScore);
+        }
+        DisplayScores();
+    }
+
+    /// <summary>
+    /// 点数を画面に表示する
+    /// </summary>
+    private void DisplayScores()
+    {
+        scoreText.text = score.ToString();
+        highScoreText.text = highScore.ToString();
+        
+    }
+
+    /// <summary>
+    /// ハイスコアの保存
+    /// </summary>
+    public void save()
+    {
+        PlayerPrefs.SetInt(highScoreKey, highScore);
+        PlayerPrefs.Save();
+        Initialize();
+    }
+    
+}
